7246dfa57f2c122b724df262a042ca42b80e74fb
Veld <velddev@gmail.com>
2019-02-03 00:08:07 +0100

    removed js files

--- mix.exs
+++ mix.exs
@@ -1,3 +1,20 @@
+defmodule Consumer do
+    use GenStage
+
+    def init(_) do
+       producers = Crux.Gateway.Connection.Producer.producers() |> Map.values()
+       {:consumer, nil, subscribe_to: producers}
+    end
+
+     def handle_events(events, _from, nil) do
+       for {:MESSAGE_CREATE, message, _shard_id} <- events do
+         IO.puts("#{message.author.username}##{message.author.discriminator}: #{message.content}")
+       end
+
+       {:noreply, [], nil}
+     end
+end
+
 defmodule Fragment.MixProject do
     use Mix.Project
 
@@ -11,7 +28,7 @@ defmodule Fragment.MixProject do
     end
 
     def application() do
-        IO.puts "hello"
+
     end
 
     defp deps() do
